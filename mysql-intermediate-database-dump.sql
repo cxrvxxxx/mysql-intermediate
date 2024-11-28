85613a2275e6bc60433414a67bbad1fb21ff9adb
cxrvxxxx <brentempasisx3@gmail.com>
2024-11-28 15:01:23 +0800

    first commit

---
+++ mysql-intermediate-database-dump.sql
@@ -0,0 +1,214 @@
+-- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
+--
+-- Host: localhost    Database: dbwildpark
+-- ------------------------------------------------------
+-- Server version	9.0.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tblaccount`
+--
+
+DROP TABLE IF EXISTS `tblaccount`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tblaccount` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(255) DEFAULT NULL,
+  `lastname` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tblaccount`
+--
+
+LOCK TABLES `tblaccount` WRITE;
+/*!40000 ALTER TABLE `tblaccount` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tblaccount` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tblbooking`
+--
+
+DROP TABLE IF EXISTS `tblbooking`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tblbooking` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `date` datetime DEFAULT NULL,
+  `parking_area_id` int DEFAULT NULL,
+  `vehicle_id` int DEFAULT NULL,
+  `booker_id` int DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `parking_area_id` (`parking_area_id`),
+  KEY `vehicle_id` (`vehicle_id`),
+  KEY `booker_id` (`booker_id`),
+  CONSTRAINT `tblbooking_ibfk_1` FOREIGN KEY (`parking_area_id`) REFERENCES `tblparkingarea` (`id`),
+  CONSTRAINT `tblbooking_ibfk_2` FOREIGN KEY (`vehicle_id`) REFERENCES `tblvehicle` (`id`),
+  CONSTRAINT `tblbooking_ibfk_3` FOREIGN KEY (`booker_id`) REFERENCES `tblaccount` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tblbooking`
+--
+
+LOCK TABLES `tblbooking` WRITE;
+/*!40000 ALTER TABLE `tblbooking` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tblbooking` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tblparkingarea`
+--
+
+DROP TABLE IF EXISTS `tblparkingarea`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tblparkingarea` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `slots` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tblparkingarea`
+--
+
+LOCK TABLES `tblparkingarea` WRITE;
+/*!40000 ALTER TABLE `tblparkingarea` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tblparkingarea` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tblvehicle`
+--
+
+DROP TABLE IF EXISTS `tblvehicle`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tblvehicle` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `color` varchar(255) DEFAULT NULL,
+  `make` varchar(255) DEFAULT NULL,
+  `model` varchar(255) DEFAULT NULL,
+  `plate_number` varchar(255) DEFAULT NULL,
+  `owner_id` int DEFAULT NULL,
+  `parking_area_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `owner_id` (`owner_id`),
+  KEY `parking_area_id` (`parking_area_id`),
+  CONSTRAINT `tblvehicle_ibfk_1` FOREIGN KEY (`owner_id`) REFERENCES `tblaccount` (`id`),
+  CONSTRAINT `tblvehicle_ibfk_2` FOREIGN KEY (`parking_area_id`) REFERENCES `tblparkingarea` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tblvehicle`
+--
+
+LOCK TABLES `tblvehicle` WRITE;
+/*!40000 ALTER TABLE `tblvehicle` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tblvehicle` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Temporary view structure for view `vw_bookingdetails`
+--
+
+DROP TABLE IF EXISTS `vw_bookingdetails`;
+/*!50001 DROP VIEW IF EXISTS `vw_bookingdetails`*/;
+SET @saved_cs_client     = @@character_set_client;
+/*!50503 SET character_set_client = utf8mb4 */;
+/*!50001 CREATE VIEW `vw_bookingdetails` AS SELECT 
+ 1 AS `booking_id`,
+ 1 AS `date`,
+ 1 AS `status`,
+ 1 AS `booker_id`,
+ 1 AS `booker_firstname`,
+ 1 AS `booker_lastname`,
+ 1 AS `vehicle_id`,
+ 1 AS `plate_number`,
+ 1 AS `parking_area_id`*/;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Temporary view structure for view `vw_vehicleswithowners`
+--
+
+DROP TABLE IF EXISTS `vw_vehicleswithowners`;
+/*!50001 DROP VIEW IF EXISTS `vw_vehicleswithowners`*/;
+SET @saved_cs_client     = @@character_set_client;
+/*!50503 SET character_set_client = utf8mb4 */;
+/*!50001 CREATE VIEW `vw_vehicleswithowners` AS SELECT 
+ 1 AS `vehicle_id`,
+ 1 AS `color`,
+ 1 AS `make`,
+ 1 AS `model`,
+ 1 AS `plate_number`,
+ 1 AS `owner_id`,
+ 1 AS `owner_firstname`,
+ 1 AS `owner_lastname`*/;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Final view structure for view `vw_bookingdetails`
+--
+
+/*!50001 DROP VIEW IF EXISTS `vw_bookingdetails`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `vw_bookingdetails` AS select `b`.`id` AS `booking_id`,`b`.`date` AS `date`,`b`.`status` AS `status`,`a`.`id` AS `booker_id`,`a`.`firstname` AS `booker_firstname`,`a`.`lastname` AS `booker_lastname`,`v`.`id` AS `vehicle_id`,`v`.`plate_number` AS `plate_number`,`pa`.`id` AS `parking_area_id` from (((`tblbooking` `b` join `tblaccount` `a` on((`b`.`booker_id` = `a`.`id`))) join `tblvehicle` `v` on((`b`.`vehicle_id` = `v`.`id`))) join `tblparkingarea` `pa` on((`b`.`parking_area_id` = `pa`.`id`))) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+
+--
+-- Final view structure for view `vw_vehicleswithowners`
+--
+
+/*!50001 DROP VIEW IF EXISTS `vw_vehicleswithowners`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `vw_vehicleswithowners` AS select `v`.`id` AS `vehicle_id`,`v`.`color` AS `color`,`v`.`make` AS `make`,`v`.`model` AS `model`,`v`.`plate_number` AS `plate_number`,`a`.`id` AS `owner_id`,`a`.`firstname` AS `owner_firstname`,`a`.`lastname` AS `owner_lastname` from (`tblvehicle` `v` join `tblaccount` `a` on((`v`.`owner_id` = `a`.`id`))) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-11-28 15:00:21
---
+++ queries.sql
@@ -0,0 +1,292 @@
+DROP DATABASE dbwildpark;
+-- Create database
+CREATE DATABASE dbwildpark;
+USE dbwildpark;
+-- Create account table
+CREATE TABLE tblaccount (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    firstname VARCHAR(255),
+    lastname VARCHAR(255)
+);
+-- Create parking area table
+CREATE TABLE tblparkingarea (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    slots INT
+);
+-- Create vehicle table
+CREATE TABLE tblvehicle (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    color VARCHAR(255),
+    make VARCHAR(255),
+    model VARCHAR(255),
+    plate_number VARCHAR(255),
+    owner_id INT,
+    parking_area_id INT,
+    FOREIGN KEY (owner_id) REFERENCES tblaccount(id),
+    FOREIGN KEY (parking_area_id) REFERENCES tblparkingarea(id)
+);
+-- Create booking table
+CREATE TABLE tblbooking (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    date DATETIME,
+    parking_area_id  INT,
+    vehicle_id INT,
+    booker_id INT,
+    status VARCHAR(255),
+    FOREIGN KEY (parking_area_id) REFERENCES tblparkingarea(id),
+    FOREIGN KEY (vehicle_id) REFERENCES tblvehicle(id),
+    FOREIGN KEY (booker_id) REFERENCES tblaccount(id)
+);
+-- Stored procedure for inserting an account
+DELIMITER //
+CREATE PROCEDURE sp_InsertAccount(
+    IN p_firstname VARCHAR(255),
+    IN p_lastname VARCHAR(255),
+    OUT p_inserted_id INT
+)
+BEGIN
+    INSERT INTO tblaccount (firstname, lastname) VALUES (p_firstname, p_lastname);
+    
+    -- Retrieve the last inserted ID and store it in the OUT parameter
+    SET p_inserted_id = LAST_INSERT_ID();
+END //
+-- Stored procedure for updating an account.
+CREATE PROCEDURE sp_UpdateAccount(
+    IN p_account_id INT,
+    IN p_firstname VARCHAR(255),
+    IN p_lastname VARCHAR(255),
+    OUT p_new_firstname VARCHAR(255),
+    OUT p_new_lastname VARCHAR(255)
+)
+BEGIN
+    UPDATE tblaccount SET firstname = p_firstname, lastname = p_lastname WHERE id = p_account_id;
+    
+    SELECT firstname INTO p_new_firstname FROM tblaccount WHERE id = p_account_id;
+    SELECT lastname INTO p_new_lastname FROM tblaccount WHERE id = p_account_id;
+END //
+-- Stored procedure for deleting an account.
+CREATE PROCEDURE sp_DeleteAccount(
+    IN p_account_id INT
+)
+BEGIN
+    DELETE FROM tblaccount WHERE id = p_account_id;
+END //
+-- Stored procedure for inserting a vehicle
+DELIMITER //
+CREATE PROCEDURE sp_InsertVehicle(
+    IN p_color VARCHAR(255),
+    IN p_make VARCHAR(255),
+    IN p_model VARCHAR(255),
+    IN p_plate_number VARCHAR(255),
+    IN p_owner_id INT,
+    IN p_parking_area_id INT
+)
+BEGIN
+    INSERT INTO tblvehicle (color, make, model, plate_number, owner_id, parking_area_id)
+    VALUES (p_color, p_make, p_model, p_plate_number, p_owner_id, p_parking_area_id);
+END //
+-- Stored Procedure for updating a vehicle
+DELIMITER //
+CREATE PROCEDURE sp_UpdateVehicle(
+    IN p_vehicle_id INT,
+    IN p_color VARCHAR(255),  
+    IN p_plate_number VARCHAR(255)
+)
+BEGIN
+    UPDATE tblvehicle
+    SET
+        color = COALESCE(p_color, color),
+        plate_number = COALESCE(p_plate_number, plate_number)
+    WHERE
+        id = p_vehicle_id;
+END //
+-- Stored procedure for deleting a vehicle
+DELIMITER //
+CREATE PROCEDURE sp_DeleteVehicle(
+    IN p_owner_id INT
+)
+BEGIN
+    DELETE FROM tblvehicle WHERE id = p_owner_id;
+END //
+-- Stored procedure for updating a booking status
+DELIMITER //
+CREATE PROCEDURE sp_UpdateBookingStatus(
+    IN p_booking_id INT,
+    IN p_status VARCHAR(255)
+)
+BEGIN
+    UPDATE tblbooking SET status = p_status WHERE id = p_booking_id;
+END //
+-- Trigger on Account Deletion
+DELIMITER //
+CREATE TRIGGER tr_AfterDeleteAccount
+AFTER DELETE ON tblaccount
+FOR EACH ROW
+BEGIN
+    DELETE FROM tblvehicle WHERE owner_id = OLD.id;
+END //
+
+DELIMITER ;
+-- Vehicle and Owner View
+CREATE VIEW vw_VehiclesWithOwners AS
+SELECT
+    v.id AS vehicle_id,
+    v.color,
+    v.make,
+    v.model,
+    v.plate_number,
+    a.id AS owner_id,
+    a.firstname AS owner_firstname,
+    a.lastname AS owner_lastname
+FROM
+    tblvehicle v
+    JOIN tblaccount a ON v.owner_id = a.id;
+-- Booking Details View
+CREATE VIEW vw_BookingDetails AS
+SELECT
+    b.id AS booking_id,
+    b.date,
+    b.status,
+    a.id AS booker_id,
+    a.firstname AS booker_firstname,
+    a.lastname AS booker_lastname,
+    v.id AS vehicle_id,
+    v.plate_number,
+    pa.id AS parking_area_id
+FROM
+    tblbooking b
+    JOIN tblaccount a ON b.booker_id = a.id
+    JOIN tblvehicle v ON b.vehicle_id = v.id
+    JOIN tblparkingarea pa ON b.parking_area_id = pa.id;
+
+-- BOOKINGS PROCEDURES
+DELIMITER $$$
+CREATE PROCEDURE sp_CreateBooking(
+    IN p_date DATETIME,
+    IN p_parking_area_id INT,
+    IN p_vehicle_id INT,
+    IN p_booker_id INT
+)
+BEGIN
+    DECLARE isVehicleOwner INT DEFAULT 0;
+    DECLARE isVehicleBooked INT DEFAULT 0;
+
+    -- Check if vehicle belongs to the booker
+    SELECT COUNT(*) INTO isVehicleOwner FROM vw_VehiclesWithOwners 
+    WHERE vehicle_id = p_vehicle_id AND owner_id = p_booker_id;
+
+    -- Check if vehicle is already booked
+    SELECT COUNT(*) INTO isVehicleBooked FROM tblvehicle
+    WHERE id = p_vehicle_id AND parking_area_id IS NOT NULL;
+
+    IF isVehicleOwner > 0 AND isVehicleBooked = 0 THEN
+        INSERT INTO tblbooking (date, parking_area_id, vehicle_id, booker_id, status) 
+        VALUES (p_date, p_parking_area_id, p_vehicle_id, p_booker_id, 'Pending');
+
+    ELSEIF isVehicleBooked > 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Vehicle is already booked.';
+    ELSE 
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = "Booker does not own this vehicle";
+    END IF;
+END$$$
+DELIMITER ;
+
+-- TRIGGER WHEN BOOKING IS MADE
+DELIMITER $$$
+CREATE TRIGGER tr_AfterInsertBooking 
+AFTER INSERT ON tblbooking
+FOR EACH ROW
+BEGIN
+    UPDATE tblparkingarea
+    SET slots = slots - 1
+    WHERE id = NEW.parking_area_id;
+
+    UPDATE tblvehicle
+    SET parking_area_id = NEW.parking_area_id
+    WHERE id = NEW.vehicle_id;
+END $$$
+DELIMITER ;
+
+-- TRIGGER WHEN BOOKING IS PAID
+DELIMITER $$$
+CREATE TRIGGER tr_AfterUpdateBooking 
+AFTER UPDATE ON tblbooking
+FOR EACH ROW
+BEGIN
+    IF OLD.status != 'Paid' AND NEW.status = 'Paid' THEN
+        UPDATE tblparkingarea
+        SET slots = slots + 1
+        WHERE id = OLD.parking_area_id;
+
+        UPDATE tblvehicle
+        SET parking_area_id = NULL
+        WHERE id = OLD.vehicle_id;
+    END IF;
+END $$$
+DELIMITER ;
+
+-- TRIGGER WHEN BOOKING IS DELETED
+DELIMITER $$$
+CREATE TRIGGER tr_AfterDeleteBooking
+AFTER DELETE ON tblbooking
+FOR EACH ROW
+BEGIN
+    UPDATE tblparkingarea
+    SET slots = slots + 1
+    WHERE id = OLD.parking_area_id;
+
+    UPDATE tblvehicle
+    SET parking_area_id = NULL
+    WHERE id = OLD.vehicle_id;
+END $$$
+DELIMITER;
+
+DELIMITER //
+CREATE PROCEDURE sp_UpdateParkingarea(
+    IN p_parking_area_id INT,
+    IN p_slots INT
+)
+BEGIN
+    UPDATE tblparkingarea
+    SET slots = p_slots
+    WHERE id = p_parking_area_id;
+END //
+
+DELIMITER //
+CREATE PROCEDURE sp_GetAllParkingAreas()
+BEGIN
+    SELECT * FROM tblparkingarea;
+END //
+
+DELIMITER //
+CREATE PROCEDURE sp_InsertParkingArea(
+    IN p_slots INT
+)
+BEGIN
+    INSERT INTO tblparkingarea (slots)
+    VALUES (p_slots);
+END //
+
+DELIMITER //
+CREATE PROCEDURE sp_DeleteParkingArea(
+    IN p_parking_area_id INT
+)
+BEGIN
+    DELETE FROM tblparkingarea
+    WHERE id = p_parking_area_id;
+END //
+
+DELIMITER ;
+-- Procedure Calls
+SET @last_insert_id = 0;
+SET @updated_firstname = "";
+SET @updated_lastname = "";
+
+CALL sp_InsertAccount("John", "Doe", @last_insert_id);
+CALL sp_UpdateAccount(1, "Jane", "Doe", @updated_firstname, @updated_lastname);
+CALL sp_DeleteAccount(1);
+CALL sp_InsertVehicle("Matte Light Brown", "Yamaha", "Sniper 155R", "G86NG", NULL, NULL);
+CALL sp_UpdateVehicle(1, "Light Brown", "123ABC");
+CALL sp_DeleteVehicle(1);
